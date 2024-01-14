0aa5968d4c9414365bccf13cb95d06ca0632f0eb
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-14 14:26:34 +0800

    updated index.html, script.js Lesson 2: Variables

--- week5/assets/js/script.js
+++ week5/assets/js/script.js
@@ -1,1 +1,9 @@
-// FOR THE TYPING ANIMATION //
+// FOR THE TYPING ANIMATION (HOME) //
+
+var typed = new Typed(".typing_text", {
+    strings: ["CS Student", "RFS President", "APC Band Vocalist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
