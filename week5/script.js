d4aca4fb3f7e2dae38072d660a6bc023e6534e10
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-14 21:38:28 +0800

    updated script.js Lesson 7: Arrays

--- week5/script.js
+++ week5/script.js
@@ -1,7 +1,7 @@
 // FOR THE TYPING ANIMATION (HOME) //
 
 var typed = new Typed(".typing_text", {
-    strings: ["CS Student", "RFS President"],
+    strings: ["CS Student", "RFS President", "APC Band Vocalist"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
