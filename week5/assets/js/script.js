df5880dc5e8d16bed27f5fb739fd4980e3960dc6
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-14 14:30:45 +0800

    updated script.js Lesson 3: Booleans

--- week5/assets/js/script.js
+++ week5/assets/js/script.js
@@ -4,5 +4,6 @@ var typed = new Typed(".typing_text", {
     strings: ["CS Student", "RFS President", "APC Band Vocalist"],
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 1000
+    backDelay: 1000,
+    loop: true
 })
