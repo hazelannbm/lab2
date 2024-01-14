836b19c5e34aa4b77096473a444587736ed3da95
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-14 19:15:25 +0800

    updated script_about.html

---
+++ week5/script_about.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing_text2", {
+    strings: ["Me", "My Life", "My Interests", "My Hobbies"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
