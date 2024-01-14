df291c3907f9916862ce352db9429f928f29b101
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-14 21:56:30 +0800

    updated

--- week5/script_about.js
+++ week5/script_about.js
@@ -5,3 +5,11 @@ var typed = new Typed(".typing_text2", {
     backDelay: 1000,
     loop: true
 })
+
+function showNotFunctionalDialog() {
+    alert("Not functional yet.");
+}
+
+document.getElementById("skillsLink").addEventListener("click", showNotFunctionalDialog);
+document.getElementById("portfolioLink").addEventListener("click", showNotFunctionalDialog);
+document.getElementById("contactLink").addEventListener("click", showNotFunctionalDialog);
