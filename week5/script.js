9396f48e6fa21eb343f44f0e2e13f5f6069ee521
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-24 10:53:26 +0800

    updated script.js

--- week5/script.js
+++ week5/script.js
@@ -14,7 +14,6 @@ function showNotFunctionalDialog() {
 
 document.getElementById("skillsLink").addEventListener("click", showNotFunctionalDialog);
 document.getElementById("portfolioLink").addEventListener("click", showNotFunctionalDialog);
-document.getElementById("contactLink").addEventListener("click", showNotFunctionalDialog);
 
 function myFunction() {
     alert("Not functional yet.")
