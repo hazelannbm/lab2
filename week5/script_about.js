ec568d89df933810955501c9e4ee1fafc3d9a25e
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-24 11:05:29 +0800

    updated script_about.js

--- week5/script_about.js
+++ week5/script_about.js
@@ -1,11 +1,3 @@
-var typed = new Typed(".typing_text2", {
-    strings: ["Me", "My Life", "My Interests", "My Hobbies"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
-
 function showNotFunctionalDialog() {
     alert("Not functional yet.");
 }
