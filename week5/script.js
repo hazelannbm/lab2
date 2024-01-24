e045da3cd2ce451f4f3579a87e87a97b90d2d35e
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-24 11:08:01 +0800

    updated script.js

--- week5/script.js
+++ week5/script.js
@@ -1,13 +1,5 @@
 // FOR THE TYPING ANIMATION (HOME)
 
-var typed = new Typed(".typing_text", {
-    strings: ["CS Student", "RFS President", "APC Band Vocalist"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
-
 function showNotFunctionalDialog() {
     alert("Not functional yet.");
 }
--- week5/script_about.js
+++ week5/script_about.js
@@ -1,3 +1,11 @@
+var typed = new Typed(".typing_text2", {
+    strings: ["Me", "My Life", "My Interests", "My Hobbies"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
 function showNotFunctionalDialog() {
     alert("Not functional yet.");
 }
