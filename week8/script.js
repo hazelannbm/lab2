96a48be1015c8bbcb1521e81724a349a342c2dc5
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-25 16:11:52 +0800

    updated skills.html PHP Lesson 3: Echo/Print

--- week8/script.js
+++ week8/script.js
@@ -12,7 +12,6 @@ function showNotFunctionalDialog() {
     alert("Not functional yet.");
 }
 
-document.getElementById("skillsLink").addEventListener("click", showNotFunctionalDialog);
 document.getElementById("portfolioLink").addEventListener("click", showNotFunctionalDialog);
 document.getElementById("contactLink").addEventListener("click", showNotFunctionalDialog);
 
--- week8/script_about.js
+++ week8/script_about.js
@@ -10,6 +10,5 @@ function showNotFunctionalDialog() {
     alert("Not functional yet.");
 }
 
-document.getElementById("skillsLink").addEventListener("click", showNotFunctionalDialog);
 document.getElementById("portfolioLink").addEventListener("click", showNotFunctionalDialog);
 document.getElementById("contactLink").addEventListener("click", showNotFunctionalDialog);
