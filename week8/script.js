bef23d3f22dacbe3af1183b3b9e5f8042b4535cd
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-01-25 15:18:36 +0800

    updated week8 folder

---
+++ week8/script.js
@@ -0,0 +1,32 @@
+// FOR THE TYPING ANIMATION (HOME)
+
+var typed = new Typed(".typing_text", {
+    strings: ["CS Student", "RFS President", "APC Band Vocalist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+function showNotFunctionalDialog() {
+    alert("Not functional yet.");
+}
+
+document.getElementById("skillsLink").addEventListener("click", showNotFunctionalDialog);
+document.getElementById("portfolioLink").addEventListener("click", showNotFunctionalDialog);
+document.getElementById("contactLink").addEventListener("click", showNotFunctionalDialog);
+
+function myFunction() {
+    alert("Not functional yet.")
+}
+
+function myFunction2() {
+    alert("You are viewing my webpage.")
+}
+
+function myFunction3() {
+    alert("Good day.")
+}
+
+myFunction3();
+myFunction2();
---
+++ week8/script_about.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing_text2", {
+    strings: ["Me", "My Life", "My Interests", "My Hobbies"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+function showNotFunctionalDialog() {
+    alert("Not functional yet.");
+}
+
+document.getElementById("skillsLink").addEventListener("click", showNotFunctionalDialog);
+document.getElementById("portfolioLink").addEventListener("click", showNotFunctionalDialog);
+document.getElementById("contactLink").addEventListener("click", showNotFunctionalDialog);
