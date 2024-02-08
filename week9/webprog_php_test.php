3adf1adb23b1b70b6ae992635dd455076ac15c8d
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2024-02-08 14:35:15 +0800

    updated webprog_php_test.php

--- week9/webprog_php_test.php
+++ week9/webprog_php_test.php
@@ -88,5 +88,30 @@ echo "<br>";
 echo $gender;
 ?>
 
+<?php
+$servername = "localhost";
+$username = "webprogss221";
+$password = "d=latHen97";
+$dbname = "webprogss221";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO hbmones_myguests (firstname, lastname, email)
+VALUES ('$name', '$website', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
 </body>
 </html>
